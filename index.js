b39d5e8ba3ffef5a2a9fdbf666d81d51304f5665
Alex Lei <97415826+AlexLei1@users.noreply.github.com>
2022-11-10 10:00:45 +0300

    Работает не дальше 3 стр, появилась ветка целевого

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // @name         GoogleBOT
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  BOT for Google
+// @description  Bot for Google
 // @author       Alexander Churkin
-// @match        https://www.google.ru/
+// @match        https://www.google.ru/*
+// @match        https://www.napli.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -13,21 +14,44 @@ let links = document.links;
 let btnK = document.getElementsByName("btnK")[0];
 let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress","Вывод произвольных типов записей и полей в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
+let googleInput = document.getElementsByName("q")[0]
 
 if (btnK !== undefined) {
-	document.getElementsByName("q")[0].value = keyword;
-	btnK.click();
-	} else {
-	for (let i = 0; i < links.length; i++) {
-		if (links[i].href.indexOf("napli.ru") !== -1) {
-			let link = links[i];
-			console.log("Нашел строку " + link);
-			link.click();
-			break;
+  let i = 0;
+	let timerId = setInterval(() => {
+		googleInput.value += keyword[i]
+		i++
+		if (i == keyword.length) {
+			clearInterval(timerId) 
+			btnK.click() 
 		}
+	}, 100)
+} else if (location.hostname === 'napli.ru'){
+	
+} else {
+	let nextGooglePage = true;
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("napli.ru") !== -1) {
+      let link = links[i];
+			nextGooglePage = false;
+      console.log("Нашел строку " + link);
+      setTimeout(()=>{
+				link.click();
+			},getRandom(1500, 4000));
+      break;
+    }
+  }
+	if (document.querySelector('YyVfkd').innerText == '5'){
+		nextGooglePage = false;
+		location.href = 'https://www.google.ru/'
+	}
+	if (nextGooglePage) {
+		setTimeout(()=>{
+			pnnext.click();
+		},getRandom(1500, 4000));
 	}
 }
 
 function getRandom(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
